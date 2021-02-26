cc9ee7bb5b0d305ab7c317f7d4ea80bbcfd15530
HA3YA <ha3ya@t-online.hu>
2021-02-26 21:36:58 +0100

    Added: Drop Current QSO, Drop All Dynamic

--- Readme.md
+++ Readme.md
@@ -1,33 +1,83 @@
 # DMR Talkgroup Switch Home Assistant component
 
-Custom component for [Home Assistant](https://homeassistant.io) so you can ADD, DELETE static talkgroups to a hotspot on a Brandmeister Server.
-![Screenshot](screenshot.png?raw=true)
+Custom component for [Home Assistant](https://homeassistant.io) so you can   
+ - ADD, DELETE static talkgroups to a hotspot  
+ - Drop Current QSO  
+ - Drop All Dynamic Talkgroups 
+ 
+on a Brandmeister DMR Server.   
+
+![Screenshot](screenshot_2.png?raw=true)
+![Screenshot](screenshot_1.png?raw=true)
 
 ## Installation
 
-1. Copy the folder `dmr_tg_switch` to `custom_components` inside your Home Assistant config folder.
+1. Copy the folder `dmr_tg_switch` to `custom_components` inside your Home Assistant config folder
 2. Restart Home Assistant (this installs the component's dependencies)
 3. Add your config to `configuration.yaml` (see options below)
 4. Restart Home Assistant again
 
 ## Configuration
 
+### Talkgroup switch
 ``` yaml
 switch:
   - platform: dmr_tg_switch
     name: TG216
-    dmr_id: 216999901                       
+    dmr_id: 123456701                       
     bm_api_key: !secret your_bm_api_key     
     tg: 216                                 
     tslot: 1                                  
     scan_interval:                                
-      hours: 1
-```
+      hours: 1        
+```     
 
-`name`:          Talkgroup friendly name      
-`dmr_id`:        Your 7 characters DMR ID + 01..99 (more hotspots) ie. 216999901      
-`bm_api_key`:    Your Brandmeister API Key    
-`tg`:            Talkgroup to ADD/DEL    
-`tslot`:         TimeSlot [0/1/2], simplex hotspot=0, duplex hotspot=1/2    
-`scan_interval`: Updating every hour (instead of the default 30 seconds) seems to be OK.   
+|   |    |   
+| :--- | :---- |  
+|`name`       | Talkgroup friendly name |
+|`dmr_id`     | Your 7-digit personal DMR ID + 01...99 suffix for more than one hotspot (for example: 2169999**01**) |   
+|`bm_api_key` | Your Brandmeister API Key |
+|`tg`         | Talkgroup to ADD/DEL |
+|             | 9998 = Drop Current QSO | 
+|             | 9997 = Drop All Dynamic Talkgroups | 
+|`tslot` | TimeSlot [0/1/2], simplex hotspot=0, duplex hotspot=1/2 |   
+|`scan_interval` | Updating every hour (instead of the default 30 seconds) seems to be OK. |  
+    
+        
+### Drop Current QSO
+``` yaml
+switch:
+  - platform: dmr_tg_switch
+    name: Drop Current QSO S1
+    dmr_id: 123456701                       
+    bm_api_key: !secret your_bm_api_key     
+    tg: 9998                                 
+    tslot: 1                                  
+    scan_interval:                                
+      hours: 1        
+``` 
+Create it as a button in HA Lovelace.   
+
+![Screenshot](drop_current_2.png?raw=true)
+![Screenshot](drop_current_1.png?raw=true)
+
+
+
+### Drop All Dynamic Talkgroups
+``` yaml
+switch:
+  - platform: dmr_tg_switch
+    name: Drop All Dynamic S1
+    dmr_id: 123456701                       
+    bm_api_key: !secret your_bm_api_key     
+    tg: 9997                                 
+    tslot: 1                                  
+    scan_interval:                                
+      hours: 1        
+```  
+    
+Create it as a button in HA Lovelace.   
+   
+![Screenshot](drop_all_dynamic_2.png?raw=true)
+![Screenshot](drop_all_dynamic_1.png?raw=true)
 
--- switch.py
+++ switch.py
@@ -1,5 +1,8 @@
 import logging
 from homeassistant.helpers.entity import ToggleEntity
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.switch import PLATFORM_SCHEMA
 from homeassistant.const import CONF_NAME
 
 import requests
@@ -18,7 +21,20 @@ CONF_SLOT = 'tslot'
 DEFAULT_NAME = 'DMR TG Switch'
 DEFAULT_ICON = 'mdi:account-multiple'
 
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_DMR_ID): cv.positive_int,
+        vol.Required(CONF_BM_API_KEY): cv.string,
+        vol.Required(CONF_TG): cv.positive_int,
+        vol.Required(CONF_SLOT): cv.positive_int,
+        vol.Optional(CONF_NAME, DEFAULT_NAME): cv.string
+   }
+)
+
 URL_BM = "https://api.brandmeister.network/v1.0/repeater/"
+
+DROP_CUR_QSO = 9998         # TG for Drop Current QSO 
+DROP_DYN_TGS = 9997         # TG for Drop All Dynamic TG    
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     dmr_id = config.get(CONF_DMR_ID)
@@ -33,6 +49,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 
 class DMRTalkgroupSwitch(ToggleEntity):
+
     def __init__(self, dmr_id: int, bm_api_key: str, tg: int, tslot: int, name: str):
         self._state = None
         self._is_on = False
@@ -53,39 +70,47 @@ class DMRTalkgroupSwitch(ToggleEntity):
 
     #@property
     def turn_on(self, **kwargs):
-        self._is_on = True
-        # Send ADD command to BM
-        url = URL_BM + "talkgroup/?action=ADD&id=" + str(self.dmr_id) 
-        data = "talkgroup=" + str(self.tg) + "&timeslot=" + str(self.tslot)
-        header = {'Content-Length': str(len(data)),
-                  'Content-Type': 'application/x-www-form-urlencoded'
-                 }
-        response_api = requests.post(url, data=data, auth=HTTPBasicAuth(self.bm_api_key, ''), headers=header)         
-        self.update()
+        if self.tg == DROP_CUR_QSO :
+            url = URL_BM + "setRepeaterDbus.php?action=dropCallRoute&slot=" + str(self.tslot) + "&q=" + str(self.dmr_id) 
+            response_api = requests.post(url, auth=HTTPBasicAuth(self.bm_api_key, ''))         
+            _LOGGER.warning('Command to BM: Drop Current QSO')
+        elif self.tg == DROP_DYN_TGS :
+            url = URL_BM + "setRepeaterTarantool.php?action=dropDynamicGroups&slot=" + str(self.tslot) + "&q=" + str(self.dmr_id) 
+            response_api = requests.post(url, auth=HTTPBasicAuth(self.bm_api_key, ''))         
+            #_LOGGER.warning("Command to BM: Drop All Dynamic TG\'s")
+        else:
+            self._is_on = True
+            url = URL_BM + "talkgroup/?action=ADD&id=" + str(self.dmr_id) 
+            data = "talkgroup=" + str(self.tg) + "&timeslot=" + str(self.tslot)
+            header = {'Content-Length': str(len(data)),
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                     }
+            response_api = requests.post(url, data=data, auth=HTTPBasicAuth(self.bm_api_key, ''), headers=header)         
+            #_LOGGER.warning('Command to BM: ADD Static Talkgroup: ' + str(self.tg))
+            self.update()
 
     #@property
     def turn_off(self, **kwargs):
         self._is_on = False
-        # Send DEL command to BM
         url = URL_BM + "talkgroup/?action=DEL&id=" + str(self.dmr_id) 
         data = "talkgroup=" + str(self.tg) + "&timeslot=" + str(self.tslot)
         header = {'Content-Length': str(len(data)),
                   'Content-Type': 'application/x-www-form-urlencoded'
                  }
         response_api = requests.post(url, data=data, auth=HTTPBasicAuth(self.bm_api_key, ''), headers=header)         
+        #_LOGGER.warning('Command to BM: DELETE Static Talkgroup: ' + str(self.tg))
         self.update()
- 
+    
     def update(self):
-        # Check BM 
-        status = 0
-        url = URL_BM + "?action=profile&q=" + str(self.dmr_id) 
-        response_api = requests.get(url) 
-        bm_data = json.loads(response_api.text)    
-        for i in bm_data['staticSubscriptions']:
-            if i["talkgroup"] == self.tg:
-                self._is_on = True 
-                status = 1
-        if status == 0:
-            self._is_on = False
-  
-
+        if self.tg < DROP_DYN_TGS or self.tg > DROP_CUR_QSO:
+            status = 0
+            url = URL_BM + "?action=profile&q=" + str(self.dmr_id) 
+            response_api = requests.get(url) 
+            bm_data = json.loads(response_api.text)    
+            for i in bm_data['staticSubscriptions']:
+                if i["talkgroup"] == self.tg:
+                    self._is_on = True 
+                    status = 1
+            if status == 0:
+                self._is_on = False
+            #_LOGGER.warning('Updated Static Talkgroup: ' + str(self.tg))
